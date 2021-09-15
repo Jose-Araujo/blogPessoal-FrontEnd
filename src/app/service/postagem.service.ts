c9f9a149e0ce4ad1402df01162829679a41a48c8
Jose-Araujo <jose.vinicius450@gmail.com>
2021-09-14 23:51:50 -0300

    Pesquisas implementadas

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -23,6 +23,10 @@ export class PostagemService {
 
   getByIdPostagem(id: number): Observable<Postagem>{
     return this.http.get<Postagem>(`https://blogjosearaujo.herokuapp.com/postagens/${id}`, this.token)
+  } 
+
+  getByTituloPostagem(titulo: string): Observable<Postagem[]>{
+    return this.http.get<Postagem[]>(`https://blogjosearaujo.herokuapp.com/postagens/titulo/${titulo}`, this.token)
   }
 
   postPostagem(postagem: Postagem): Observable<Postagem>{
